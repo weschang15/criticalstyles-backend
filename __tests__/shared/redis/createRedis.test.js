2f52cc5e5d939070d348bcced44684a9e35ea50b
Wesley Chang <weschang15@gmail.com>
2019-05-28 17:30:19 -0700

    Added test for redis helper

---
+++ __tests__/shared/redis/createRedis.test.js
@@ -0,0 +1,30 @@
+require = require("esm")(module);
+const Redis = require("ioredis-mock");
+const { getConfig } = require("../../../utils");
+
+const createRedisMock = jest.fn();
+createRedisMock.mockImplementation((opts = {}) => {
+  const config = getConfig("redis");
+  return new Redis({ ...config, ...opts, data: {} });
+});
+
+describe("createRedis", () => {
+  beforeEach(() => {
+    createRedisMock.mockClear();
+  });
+
+  it("should return one instance of Redis", () => {
+    createRedisMock();
+    expect(createRedisMock).toHaveReturnedTimes(1);
+    expect(createRedisMock.mock.results[0].value).toBeInstanceOf(Redis);
+  });
+
+  it("should allow custom configuration parameters", () => {
+    const opts = { password: "password" };
+
+    createRedisMock(opts);
+    expect(createRedisMock).toBeCalledWith(opts);
+    expect(createRedisMock).toHaveReturnedTimes(1);
+    expect(createRedisMock.mock.results[0].value).toBeInstanceOf(Redis);
+  });
+});
