b6c3bb4bf9335874ebcaeaec77af72bc5d1220c6
Wesley Chang <weschang15@gmail.com>
2019-08-06 22:08:20 -0700

    Updated helper to extract mongoose validation errors

--- utils/extractErrors.js
+++ utils/extractErrors.js
@@ -1,4 +1,15 @@
+import { DatabaseError } from "../api/services/db";
+
 const extractErrors = e => {
+  if (e instanceof DatabaseError) {
+    if ("errors" in e) {
+      return Object.values(e.errors).map(err => ({
+        path: err.path,
+        message: err.message
+      }));
+    }
+  }
+
   if (e instanceof Error) {
     return [{ path: "name", message: e.message || "Something went wrong" }];
   }
