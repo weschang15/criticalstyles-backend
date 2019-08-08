1a7b03229f9a315bfdb2c2585e629d68fe403793
Wesley Chang <weschang15@gmail.com>
2019-08-07 17:17:02 -0700

    Fixed async await

--- api/middlewares/auth/hydrateUser.js
+++ api/middlewares/auth/hydrateUser.js
@@ -1,14 +1,14 @@
-import { User } from '../../models';
-import { withCatch } from '../../../utils';
+import { User } from "../../models";
+import { withCatch } from "../../../utils";
 
-function hydrateUser(req, _, next) {
+async function hydrateUser(req, _, next) {
   const { session } = req;
   if (!session.user) {
     return next();
   }
 
   const { user } = session;
-  const currentUser = await withCatch(User.findById(user._id, '-password'));
+  const currentUser = await withCatch(User.findById(user._id, "-password"));
   // set the current User model instance on req object
   req.user = currentUser;
   return next();
