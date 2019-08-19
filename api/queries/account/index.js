92b4b91fb774be2158cfcc962b323b41e378e96d
Wesley Chang <weschang15@gmail.com>
2019-08-18 22:12:45 -0700

    Added query resolvers

--- api/queries/account/index.js
+++ api/queries/account/index.js
@@ -1,6 +1,12 @@
 import getAccount from "./getAccount";
+import sites from "./sites";
+import users from "./users";
 
 const resolvers = {
+  Account: {
+    sites,
+    users
+  },
   Query: {
     getAccount
   }
