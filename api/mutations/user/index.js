d8dd67677eb0d5043b5e051c9a94f8e292c38f94
Wesley Chang <weschang15@gmail.com>
2019-08-18 22:12:33 -0700

    Removed dead imports and resovlers

--- api/mutations/user/index.js
+++ api/mutations/user/index.js
@@ -1,12 +1,8 @@
 import createUser from "./createUser";
-import login from "./login";
-import logout from "./logout";
 
 const resolver = {
   Mutation: {
-    createUser,
-    login,
-    logout
+    createUser
   }
 };
 
