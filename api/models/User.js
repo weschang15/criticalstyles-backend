99180c8d15c13922c3507b19307210e4fcf001ae
Wesley Chang <weschang15@gmail.com>
2019-08-18 22:09:42 -0700

    Updated authentication method to return user and a default account

--- api/models/User.js
+++ api/models/User.js
@@ -2,32 +2,39 @@ import argon2 from "argon2";
 import pick from "lodash/pick";
 import { createModel, _Schema, Types } from "./Utils";
 
-const UserSchema = new _Schema({
-  firstName: {
-    type: String,
-    required: [true, "A first name must be provided."],
-    trim: true
+const UserSchema = new _Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "A first name must be provided."],
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: [true, "A last name must be provided."],
+      trim: true
+    },
+    email: {
+      type: String,
+      required: [true, "An email must be provided."],
+      trim: true,
+      unique: true,
+      index: true
+    },
+    password: {
+      type: String,
+      required: [true, "A password must be provided"],
+      minlength: 8,
+      maxlength: 1024,
+      trim: true
+    },
+    accounts: [{ type: Types.ObjectId, ref: "Account" }]
   },
-  lastName: {
-    type: String,
-    required: [true, "A last name must be provided."],
-    trim: true
-  },
-  email: {
-    type: String,
-    required: [true, "An email must be provided."],
-    trim: true,
-    unique: true,
-    index: true
-  },
-  password: {
-    type: String,
-    required: [true, "A password must be provided"],
-    minlength: 8,
-    maxlength: 1024,
-    trim: true
-  },
-  accounts: [{ type: Types.ObjectId, ref: "Account" }]
+  { toJSON: { virtuals: true }, toObject: { vrituals: true } }
+);
+
+UserSchema.virtual("defaultAccount").get(function() {
+  return this.accounts[0];
 });
 
 UserSchema.pre("save", async function(next) {
@@ -40,7 +47,7 @@ UserSchema.pre("save", async function(next) {
 });
 
 UserSchema.methods.toJSON = function() {
-  return pick(this, ["firstName", "lastName", "email", "_id", "accounts"]);
+  return pick(this, ["firstName", "lastName", "email", "_id"]);
 };
 
 UserSchema.statics.authenticate = async function({ email, password }) {
@@ -48,7 +55,7 @@ UserSchema.statics.authenticate = async function({ email, password }) {
     throw new Error("Undefined argument `password`.");
   }
 
-  const user = await this.findOne({ email }).populate("accounts", "_id");
+  const user = await this.findOne({ email }).populate("accounts");
 
   if (!user) {
     throw new Error("Invalid email or password.");
@@ -59,7 +66,7 @@ UserSchema.statics.authenticate = async function({ email, password }) {
     throw new Error("Invalid email or password.");
   }
 
-  return user.toJSON();
+  return { user: user.toJSON(), account: user.defaultAccount.toJSON() };
 };
 
 export default createModel("User", UserSchema);
