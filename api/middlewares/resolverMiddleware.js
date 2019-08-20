fe46c6765141e65538442d8f28bce3542622f821
Wesley Chang <weschang15@gmail.com>
2019-08-19 20:00:53 -0700

    Added validation middelware for creating a site

--- api/middlewares/resolverMiddleware.js
+++ api/middlewares/resolverMiddleware.js
@@ -1,7 +1,13 @@
 import merge from "lodash/merge";
 import siteMiddleware from "./site";
+import accountMiddleware from "./account";
 import stylesheetMiddelware from "./stylesheet";
 
-const resolverMiddleware = merge({}, siteMiddleware, stylesheetMiddelware);
+const resolverMiddleware = merge(
+  {},
+  accountMiddleware,
+  siteMiddleware,
+  stylesheetMiddelware
+);
 
 export default resolverMiddleware;
--- api/middlewares/site/index.js
+++ api/middlewares/site/index.js
@@ -1,11 +1,29 @@
+import { object, string } from "yup";
 import requireUser from "../auth/requireUser";
 
-const mutations = {
+const schema = object().shape({
+  name: string()
+    .trim()
+    .required(),
+  url: string()
+    .trim()
+    .url()
+    .required(),
+  accountId: string()
+    .trim()
+    .required()
+});
+
+const middleware = {
   Mutation: {
     createSite: requireUser(async (resolve, parent, args, context, info) => {
+      const validate = input => schema.validate(input, { abortEarly: false });
+      const [err] = await withCatch(validate(args.input));
+
+      if (err) return { ok: false, errors: extractErrors(err) };
       return resolve(parent, args, context, info);
     })
   }
 };
 
-export default mutations;
+export default middleware;
