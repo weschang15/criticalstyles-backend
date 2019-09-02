1273682febd753e8a5da4cb57ae09de02015ce48
Wesley Chang <weschang15@gmail.com>
2019-09-02 16:46:45 -0700

    I guess reordered imports

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 // Database connection, import the module only for its side effects
-import "./api/services/db";
-
 import { app, http } from "./api";
+import "./api/services/db";
 
 const server = http.listen(app.get("port"), () => {
   console.log(`Express running → PORT ${server.address().port}`);
