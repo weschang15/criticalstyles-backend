dfdc5e8ef5047f7127f38712db8f688f7905f244
Wesley Chang <weschang15@gmail.com>
2019-09-02 16:47:06 -0700

    Created process function for creating page ccss

---
+++ worker/queues/service/process.js
@@ -0,0 +1,17 @@
+import { Page } from "../../../api/models";
+import { cleanCSS, penthouse } from "../../../api/services";
+
+async function handleProcess({ pageId, pageUrl, viewport }) {
+  const { styles, stats } = cleanCSS(await penthouse(pageUrl, viewport));
+  await Page.findByIdAndUpdate(pageId, {
+    stylesheet: { styles, stats }
+  });
+
+  return pageId;
+}
+
+function process({ data }) {
+  return handleProcess(data);
+}
+
+export default process;
