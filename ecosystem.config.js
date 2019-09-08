bef8e73c22233022aec965ce1ee9dea1d0916b95
Wesley Chang <weschang15@gmail.com>
2019-09-07 18:14:45 -0700

    Removed worker process from pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,21 +14,6 @@ module.exports = {
       env_production: {
         NODE_ENV: "production"
       }
-    },
-    {
-      name: "WORKER",
-      script: "./worker.js",
-      node_args: "-r esm -r dotenv/config",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
     }
   ]
 };
