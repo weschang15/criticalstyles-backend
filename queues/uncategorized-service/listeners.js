e601c4716b46f1b08c3c87328c664b9d5859c3d6
Wesley Chang <weschang15@gmail.com>
2019-09-08 14:34:14 -0700

    feat: handle uncategorized ccss complete event

--- queues/uncategorized-service/listeners.js
+++ queues/uncategorized-service/listeners.js
@@ -1,7 +1,27 @@
+import { Notification } from "../../api/models";
 import createPubsub from "../../shared/redis/createPubSub";
+import { NOTIFICATION_ADDED } from "../../shared/redis/events";
 const pubsub = createPubsub();
 
-async function onCompleted(job, result) {}
+async function onCompleted(job, result) {
+  const { accountId, pageUrl, key } = JSON.parse(result);
+
+  const newNotification = new Notification({
+    account: accountId,
+    data: {
+      ccssKey: key,
+      pageUrl
+    },
+    message: `Critical CSS for ${escape(pageUrl)} successfully generated.`
+  });
+
+  const notification = await newNotification.save();
+
+  pubsub.publish(NOTIFICATION_ADDED, {
+    accountId,
+    notification
+  });
+}
 
 const listeners = {
   ["global:completed"]: onCompleted
