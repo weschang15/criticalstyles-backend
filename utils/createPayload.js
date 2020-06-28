dfdf9f2fb9591672fbdcaf44c0985052544dfc86
Wesley Chang <weschang15@gmail.com>
2020-06-28 13:35:32 -0700

    feat: utils for structured response data

---
+++ utils/createPayload.js
@@ -0,0 +1,30 @@
+import extractErrors from "./extractErrors";
+
+/**
+ * Helper function responsible for returning an object with necessary properties that satisfy the
+ * PageInfo GQL type
+ *
+ * @param {number} totalRows the number of rows returned from a SELECT COUNT(*) query
+ * @param {number} totalRowsPerPage the number of rows to limit a single page to
+ * @return {Object} pageInfo
+ */
+export const getQueryInfo = (totalRows, totalRowsPerPage = 10) => {
+  return {
+    total: totalRows,
+    totalPages: Math.ceil(totalRows / totalRowsPerPage),
+    hasNextPage: totalRows > totalRowsPerPage,
+  };
+};
+
+export function createPayload(data = {}, errors) {
+  return errors
+    ? { ok: false, errors: extractErrors(errors) }
+    : { ok: true, ...data };
+}
+
+export function createPaginatedPayload(data, errors) {
+  const { count, documents } = data;
+  const queryInfo = getQueryInfo(count);
+
+  return createPayload({ documents, queryInfo }, errors);
+}
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 export { default as createCCSSResponse } from "./createCCSSResponse";
+export * from "./createPayload";
 export { default as extractErrors } from "./extractErrors";
 export { default as getConfig } from "./getConfig";
-export { default as getQueryInfo } from "./getQueryInfo";
 export { default as removeDuplicates } from "./removeDuplicates";
 export { default as removeFalsy } from "./removeFalsy";
 export { default as withCatch } from "./withCatch";
