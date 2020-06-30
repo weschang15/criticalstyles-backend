7899f4e488c7ab32404dccd5c89c886f53f0e8af
Wesley Chang <weschang15@gmail.com>
2020-06-29 21:00:27 -0700

    fix: destructure return val from decryption

--- api/middlewares/auth/validateApiKey.js
+++ api/middlewares/auth/validateApiKey.js
@@ -2,18 +2,22 @@ import { Token } from "../../models";
 import { decryptApiKeys } from "../../services";
 
 export default async (req, res, next) => {
-  const { apiKey } = req;
-  const token = decryptApiKeys(apiKey);
-  const valid = await Token.findOne({ token });
+  try {
+    const { apiKey } = req;
+    const { token } = decryptApiKeys(apiKey);
+    const valid = await Token.findOne({ token });
 
-  if (!valid) {
+    if (!valid) {
+      throw new Error("Invalid API key");
+    }
+
+    next();
+  } catch (error) {
     return res.status(403).json({
       ok: false,
       data: {
-        message: "Invalid API key",
+        message: error.message,
       },
     });
   }
-
-  next();
 };
