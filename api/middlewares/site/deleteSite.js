6abeaccdf3919889b8aaa68e83870dea7c1e7635
Wesley Chang <weschang15@gmail.com>
2020-07-01 21:13:45 -0700

    feat: add require acc owner middleware

--- api/middlewares/site/deleteSite.js
+++ api/middlewares/site/deleteSite.js
@@ -1,19 +1,20 @@
 import { object, string } from "yup";
 import { extractErrors, withCatch } from "../../../utils";
+import requireAccountOwner from "../auth/requireAccountOwner";
 import requireUser from "../auth/requireUser";
 
 const schema = object().shape({
-  _id: string()
-    .trim()
-    .required()
+  _id: string().trim().required(),
 });
 
-export default requireUser(async (resolve, parent, args, context, info) => {
-  const validate = input => schema.validate(input, { abortEarly: false });
-  const [err, result] = await withCatch(validate(args.input));
+export default requireUser(
+  requireAccountOwner(async (resolve, parent, args, context, info) => {
+    const validate = (input) => schema.validate(input, { abortEarly: false });
+    const [err, result] = await withCatch(validate(args.input));
 
-  if (err) return { ok: false, errors: extractErrors(err) };
+    if (err) return { ok: false, errors: extractErrors(err) };
 
-  const updatedArgs = { ...args, input: { ...args.input, ...result } };
-  return resolve(parent, updatedArgs, context, info);
-});
+    const updatedArgs = { ...args, input: { ...args.input, ...result } };
+    return resolve(parent, updatedArgs, context, info);
+  })
+);
