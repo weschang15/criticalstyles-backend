9d3202dc1933a860191cd2f937f3548849a7447d
Wesley Chang <weschang15@gmail.com>
2020-07-01 18:47:29 -0700

    fix: user authentication crash when account not found upon login

--- api/mutations/auth/login.js
+++ api/mutations/auth/login.js
@@ -1,16 +1,16 @@
 import { extractErrors, withCatch } from "../../../utils";
 
 const login = async (_, { input }, { models: { User }, req }) => {
-  const authenticate = params => User.authenticate({ ...params });
-  const [error, { user, account }] = await withCatch(authenticate(input));
+  const authenticate = (params) => User.authenticate({ ...params });
+  const [error, auth] = await withCatch(authenticate(input));
 
   if (error) {
     return { ok: false, errors: extractErrors(error) };
   }
 
-  req.session.user = user;
-  req.session.account = account;
-  return { ok: true, auth: { user, account } };
+  req.session.user = auth.user;
+  req.session.account = auth.account;
+  return { ok: true, auth };
 };
 
 export default login;
