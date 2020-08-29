9542af73eef72354ea410c085c2189f63d016558
Wesley Chang <weschang15@gmail.com>
2020-08-28 21:37:46 -0700

    chore: update deps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,17 @@ module.exports = {
       name: "API",
       script: "./start.js",
       node_args: "-r esm -r dotenv/config",
-      instances: 1,
+      instances: "max",
+      exec_mode: "cluster",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
